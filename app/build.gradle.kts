66b84c71267e099d1aa85f6add0c33c40a60bd51
lonewanderer27 <adrianejames03@gmail.com>
2023-10-17 13:00:04 +0800

    build: change compileSdk to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.kicks.guesskicksnumber"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.kicks.guesskicksnumber"
